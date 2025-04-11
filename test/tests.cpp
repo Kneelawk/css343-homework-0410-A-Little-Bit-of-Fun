4b23a9eed5e67a90e52f2c7c4d89737df3ca9661
Cyan Kneelawk <kneelawk@gmail.com>
2025-04-10 19:46:51 -0700

    Add some simple BitWriter tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,6 +1,35 @@
+#include "bit_writer.h"
 #include <gtest/gtest.h>
 
-TEST(HelloTest, BasicAssertions) {
-    EXPECT_STRNE("Hello", "World");
-    EXPECT_EQ(7 * 6, 42);
+TEST(a_little_bit_of_fun, constructor) {
+    BitWriter b;
+    std::vector<uint8_t> out;
+    EXPECT_EQ(0, b.getData(out));
+    EXPECT_TRUE(out.empty());
+}
+
+TEST(a_little_bit_of_fun, write_one_bit) {
+    BitWriter b;
+    std::vector<uint8_t> out;
+    const std::vector<uint8_t> expected = {1};
+
+    b.write(true);
+
+    EXPECT_EQ(1, b.getData(out));
+    EXPECT_EQ(expected, out);
+}
+
+TEST(a_little_bit_of_fun, write_twelve_bits) {
+    BitWriter b;
+    std::vector<uint8_t> out;
+    const std::vector<uint8_t> expected = {0xAA, 0x0A};
+
+    bool bit = false;
+    for (int i = 0; i < 12; i++) {
+        b.write(bit);
+        bit = !bit;
+    }
+
+    EXPECT_EQ(2, b.getData(out));
+    EXPECT_EQ(expected, out);
 }
